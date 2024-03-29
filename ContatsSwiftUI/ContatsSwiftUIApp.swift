718b8552e4d6a67b855242c50198425670f09632
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2024-03-29 03:03:44 +0300

    Initial Commit

---
+++ ContatsSwiftUI/ContatsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContatsSwiftUIApp.swift
+//  ContatsSwiftUI
+//
+//  Created by Furkan Cingöz on 29.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContatsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
